7f0dd6e5de364f30f4b9c76e5cf1aa410a9beb19
Artem589 <abz.artem5@mail.ru>
2021-11-07 07:01:03 +0300

    simple-to-do2

--- todo.js
+++ todo.js
@@ -4,61 +4,61 @@ const list = {
   "create a task": "To Do",
   "make a bed": "To Do",
   "write a post": "In Progress",
-};
+}
 
 // Функция changeStatus - будет менять статус задачи
 function changeStatus(task, status) {
-  return (list[task] = status);
+  return list[task] = status
 }
 
-changeStatus("have a walk", "Yes");
+changeStatus("have a walk", "Yes")
 
 // Функция addTask - добавляет новую задачу
 function addTask(task) {
-  return (list[task] = "To Do");
+  return list[task] = "To Do"
 }
-addTask("have a walk");
+addTask("have a walk")
 
 // Функция deleteTask - удаляет задачу
 function deleteTask(task) {
-  return delete list[task];
+  return delete list[task]
 }
 
-deleteTask("have a walk");
+deleteTask("have a walk")
 
 // Функция showList будет выводить весь список дел в виде
 
 function showList() {
-  const listToDo = "To Do: " + "\n";
-  const listInProgress = "In Progress: " + "\n";
-  const listDone = "Done: " + "\n";
+  const listToDo = "To Do: " + "\n"
+  const listInProgress = "In Progress: " + "\n"
+  const listDone = "Done: " + "\n"
 
-  let toDo = listToDo;
-  let inProgress = listInProgress;
-  let done = listDone;
+  let toDo = listToDo
+  let inProgress = listInProgress
+  let done = listDone
 
   for (keys in list) {
     switch (list[keys]) {
       case "To Do":
-        toDo += '"' + keys + '"' + "," + "\n";
-        break;
+        toDo += '"' + keys + '"' + "," + "\n"
+        break
       case "In Progress":
-        inProgress += '"' + keys + '"' + "," + "\n";
-        break;
+        inProgress += '"' + keys + '"' + "," + "\n"
+        break
       case "Done":
-        done += '"' + keys + '"' + "," + "\n";
-        break;
+        done += '"' + keys + '"' + "," + "\n"
+        break
     }
   }
 
   if (toDo === listToDo) {
-    toDo = listToDo + "-" + "\n";
+    toDo = listToDo + "-" + "\n"
   } else if (inProgress === listInProgress) {
-    inProgress = listInProgress + "-" + "\n";
+    inProgress = listInProgress + "-" + "\n"
   } else if (done === listDone) {
-    done = listDone + "-";
+    done = listDone + "-"
   }
-  console.log(toDo + inProgress + done);
+  console.log(toDo + inProgress + done)
 }
 
-showList(list);
+showList(list)
