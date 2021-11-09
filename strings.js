953c5c575b83ccf539491715510c57935299b162
Artem589 <abz.artem5@mail.ru>
2021-11-10 00:11:41 +0300

    review

--- strings.js
+++ strings.js
@@ -1,13 +1,20 @@
 function showVerticalMessage(str) {
+  if (!str) {
+    console.log("");
+    // return;
+  }
+
+  str = str.slice(0, 10);
+
+  if (str[0] === "м") {
+    str = str[0].toUpperCase() + str.substr(1);
+  }
+
   for (i = 0; i < str.length; i++) {
-    if (str[0] === "м") {
-      str = str[0].toUpperCase() + str.substr(1);
-    } else if (str.length > 10) {
-      str = str.slice(0, 10);
-    }
     console.log(str[i]);
   }
 }
 
+// showVerticalMessage("");
 showVerticalMessage("марафон");
 // showVerticalMessage("Привет всем!!!");
