45ed427066ff358e27c477db03e90011ee1d8093
Artem589 <abz.artem5@mail.ru>
2021-11-09 13:47:20 +0300

    add string

---
+++ strings.js
@@ -0,0 +1,13 @@
+function showVerticalMessage(str) {
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === "м") {
+      str = str[0].toUpperCase() + str.substr(1);
+    } else if (str.length > 10) {
+      str = str.slice(0, 10);
+    }
+    console.log(str[i]);
+  }
+}
+
+showVerticalMessage("марафон");
+// showVerticalMessage("Привет всем!!!");
--- todo.js
+++ todo.js
@@ -1,68 +1,64 @@
-// Простой TODO лист без графического интерфейса.
+// const list = {
+//   "create a task": "To Do",
+//   "make a bed": "To Do",
+//   "write a post": "In Progress",
+// }
 
-const list = {
-  "create a task": "To Do",
-  "make a bed": "To Do",
-  "write a post": "In Progress",
-}
+// function changeStatus(task, status) {
+//   return list[task] = status
+// }
 
-// Функция changeStatus - будет менять статус задачи
-function changeStatus(task, status) {
-  return list[task] = status
-}
+// function addTask(task) {
+//   return list[task] = "To Do"
+// }
 
-// Функция addTask - добавляет новую задачу
-function addTask(task) {
-  return list[task] = "To Do"
-}
+// function deleteTask(task) {
+//   return delete list[task]
+// }
 
-// Функция deleteTask - удаляет задачу
-function deleteTask(task) {
-  return delete list[task]
-}
+// function showList() {
+//   const listToDo = "To Do: " + "\n"
+//   const listInProgress = "In Progress: " + "\n"
+//   const listDone = "Done: " + "\n"
 
-// Функция showList будет выводить весь список дел в виде
+//   let toDo = listToDo
+//   let inProgress = listInProgress
+//   let done = listDone
 
-function showList() {
-  const listToDo = "To Do: " + "\n"
-  const listInProgress = "In Progress: " + "\n"
-  const listDone = "Done: " + "\n"
+//   for (keys in list) {
+//     switch (list[keys]) {
+//       case "To Do":
+//         toDo += '"' + keys + '"' + "," + "\n"
+//         break
+//       case "In Progress":
+//         inProgress += '"' + keys + '"' + "," + "\n"
+//         break
+//       case "Done":
+//         done += '"' + keys + '"' + "," + "\n"
+//         break
+//     }
+//   }
 
-  let toDo = listToDo
-  let inProgress = listInProgress
-  let done = listDone
+//   if (toDo === listToDo) {
+//     toDo = listToDo + "-" + "\n"
+//   }
+//   if (inProgress === listInProgress) {
+//     inProgress = listInProgress + "-" + "\n"
+//   }
+//   if (done === listDone) {
+//     done = listDone + "-"
+//   }
+//   console.log(toDo + inProgress + done)
+// }
 
-  for (keys in list) {
-    switch (list[keys]) {
-      case "To Do":
-        toDo += '"' + keys + '"' + "," + "\n"
-        break
-      case "In Progress":
-        inProgress += '"' + keys + '"' + "," + "\n"
-        break
-      case "Done":
-        done += '"' + keys + '"' + "," + "\n"
-        break
-    }
-  }
-
-  if (toDo === listToDo) {
-    toDo = listToDo + "-" + "\n"
-  }  
-  if (inProgress === listInProgress) {
-    inProgress = listInProgress + "-" + "\n"
-  }  
-  if (done === listDone) {
-    done = listDone + "-"
-  }
-  console.log(toDo + inProgress + done)
-}
+// changeStatus("have a walk", "Done");
+// addTask("have a walk");
+// deleteTask("have a walk");
+// deleteTask("write a post");
+// deleteTask("make a bed");
+// deleteTask("create a task");
+// showList(list)
 
 
-changeStatus("have a walk", "Done");
-addTask("have a walk");
-deleteTask("have a walk");
-deleteTask("write a post");
-deleteTask("make a bed");
-deleteTask("create a task");
-showList(list)
+
+
