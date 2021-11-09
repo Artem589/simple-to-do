3c900ea5fc912641326aa93871eb2f9c5b97280d
Artem589 <abz.artem5@mail.ru>
2021-11-09 13:57:48 +0300

    update string

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 function showVerticalMessage(str) {
   for (i = 0; i < str.length; i++) {
-    if (str[i] === "м") {
+    if (str[0] === "м") {
       str = str[0].toUpperCase() + str.substr(1);
     } else if (str.length > 10) {
       str = str.slice(0, 10);
